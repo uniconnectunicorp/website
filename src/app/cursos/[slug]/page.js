5bb3de78757c983f67977387c9744c7d0b5ea4c6
Pedro Braga <pedrobraga2016@gmail.com>
2025-08-06 11:20:42 -0300

    att

--- src/app/cursos/[slug]/page.js
+++ src/app/cursos/[slug]/page.js
@@ -17,7 +17,7 @@ export default async function CourseDetails({ params }) {
   
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100">
-      <Head title={`${course.nome} | Uniconnect`} image={course.image} description={course.description} />
+      <Head title={`${course.nome} | Uniconnect`} image={course.image} description={`Você está a poucos passos de mudar sua vida profissional. Com uma formação rápida, reconhecida pelo MEC e registrada no SISTEC, você se torna ${course.nome} com validade nacional e ao finalizar ${course.response ? ` você pode emitir seu ${course.response}. ` : ' você pode emitir seu Certificado. '} Conquiste o espaço que merece no mercado — com respeito, segurança e crescimento real.`} />
       
       <CourseDetailsClient 
         course={course} 
