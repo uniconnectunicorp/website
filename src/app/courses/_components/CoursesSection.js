eb116ece9f10bddf0e64839dd5a0f9a1dfdf4831
SirBraga <pedrobraga2016@gmail.com>
2025-08-05 21:06:44 -0300

    att subir cards

--- src/app/courses/_components/CoursesSection.js
+++ src/app/courses/_components/CoursesSection.js
@@ -31,7 +31,7 @@ export default function CoursesSection({ coursesQuantity, initialCourses }) {
       };
 
     return (
-        <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16" >
+        <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-16" >
             <div className="flex flex-col md:flex-row justify-between items-center mb-12 gap-4">
 
             </div>
