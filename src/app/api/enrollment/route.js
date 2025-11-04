11d5028674f8f049c8524b84bab3ce799debaa94
SirBraga <pedrobraga2016@gmail.com>
2025-11-03 21:23:32 -0300

    att

--- src/app/api/enrollment/route.js
+++ src/app/api/enrollment/route.js
@@ -7,12 +7,10 @@ export async function POST(request) {
     
     // Configurar o transporter do nodemailer
     const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
-      secure: true,
+      service: 'gmail',
       auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
       },
     });
 
