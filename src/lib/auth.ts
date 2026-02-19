9fa4d405a338aec4f0ef0799ece39b080e17e532
Pedro Braga <pedrobraga2016@gmail.com>
2026-02-18 22:22:42 -0300

    att

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -11,5 +11,17 @@ export const auth = betterAuth({
         enabled: true,
         autoSignIn: false,
         requireEmailVerification: false,
-    }
+    },
+    session: {
+        expiresIn: 60 * 60 * 24 * 7, // 7 dias
+        updateAge: 60 * 60 * 24, // atualiza a cada 24h
+    },
+    user: {
+        additionalFields: {
+            role: {
+                type: "string",
+                defaultValue: "user",
+            },
+        },
+    },
 });
