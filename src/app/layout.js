ff216932577ca56367125198203e23ba19aff8df
Pedro Braga <pedrobraga2016@gmail.com>
2026-02-19 15:50:19 -0300

    att

--- src/app/layout.js
+++ src/app/layout.js
@@ -66,8 +66,10 @@ export default function RootLayout({
         <meta name="facebook-domain-verification" content="3ik9cwmm7vshne9568jnex2rr968zv" />
       </head>
       <body className="min-h-screen w-full bg-white antialiased  overflow-x-hidden">
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NM6JQVDX"
-          height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
+        {process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' && (
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NM6JQVDX"
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
+        )}
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
@@ -98,13 +100,15 @@ export default function RootLayout({
           </div>
         </ThemeProvider>
         {/* GTM no final do body para não bloquear renderização */}
-        <script dangerouslySetInnerHTML={{
-          __html: `setTimeout(function(){(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        {process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' && (
+          <script dangerouslySetInnerHTML={{
+            __html: `setTimeout(function(){(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-NM6JQVDX')},0)`
-        }} />
+          }} />
+        )}
       </body>
     </html>
   );
